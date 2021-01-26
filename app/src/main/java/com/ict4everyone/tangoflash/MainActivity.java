9ec458d95b37500306c9e91a32a897b38276b744
maiko <maiko@maiko.com>
2021-01-27 01:41:23 +0900

    結果画面から、もう一回ボタンによる遷移を追加

--- app/src/main/java/com/ict4everyone/tangoflash/MainActivity.java
+++ app/src/main/java/com/ict4everyone/tangoflash/MainActivity.java
@@ -102,4 +102,5 @@ public class MainActivity extends AppCompatActivity {
         // 英訳を表示
         englishLabel.setVisibility(View.VISIBLE);
     }
+
 }
--- app/src/main/java/com/ict4everyone/tangoflash/ResultActivity.java
+++ app/src/main/java/com/ict4everyone/tangoflash/ResultActivity.java
@@ -2,7 +2,9 @@ package com.ict4everyone.tangoflash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+    }
+
+    public void returnTop(View view) {
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
     }
 }
